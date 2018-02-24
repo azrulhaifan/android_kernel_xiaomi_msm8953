e01905d53084f80eea84060445677e2edb966593
Anomalchik <vidslava@yandex.ru>
2018-02-24 16:05:05 +0300

    markw: remove ifndef rules for test. mido build is broken for now

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -2,9 +2,6 @@
 #define __LINUX_MSM_CAM_SENSOR_H
 
 #include <uapi/media/msm_cam_sensor.h>
-#ifndef CONFIG_MACH_XIAOMI_MARKW
-#include <uapi/media/msm_camsensor_sdk.h>
-#endif
 
 #include <linux/compat.h>
 
@@ -74,18 +71,6 @@ struct csid_cfg_data32 {
 		compat_uptr_t csid_testmode_params;
 	} cfg;
 };
-
-#ifndef CONFIG_MACH_XIAOMI_MARKW
-struct msm_ir_led_cfg_data_t32 {
-	enum msm_ir_led_cfg_type_t cfg_type;
-	int32_t pwm_duty_on_ns;
-	int32_t pwm_period_ns;
-};
-
-struct msm_ir_cut_cfg_data_t32 {
-	enum msm_ir_cut_cfg_type_t cfg_type;
-};
-#endif
 
 struct eeprom_read_t32 {
 	compat_uptr_t dbuffer;
@@ -273,15 +258,6 @@ struct msm_flash_cfg_data_t32 {
 
 #define VIDIOC_MSM_FLASH_CFG32 \
 	_IOWR('V', BASE_VIDIOC_PRIVATE + 13, struct msm_flash_cfg_data_t32)
-
-#ifndef CONFIG_MACH_XIAOMI_MARKW
-
-#define VIDIOC_MSM_IR_LED_CFG32 \
-	_IOWR('V', BASE_VIDIOC_PRIVATE + 14, struct msm_ir_led_cfg_data_t32)
-
-#define VIDIOC_MSM_IR_CUT_CFG32 \
-	_IOWR('V', BASE_VIDIOC_PRIVATE + 15, struct msm_ir_cut_cfg_data_t32)
-#endif
 
 #endif
 
